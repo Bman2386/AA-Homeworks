c1d6a258a6e6dd9bf8835ef88c264548cb644a6e
Bman2386 <brendon.biagi@gmail.com>
2020-08-19 21:38:53 -0400

    Start HW

--- W7D3/music_app/config/environments/test.rb
+++ W7D3/music_app/config/environments/test.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -44,3 +45,6 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+  # Configure default mail server
+  Rails.application.routes.default_url_options[:host] = 'domain.com'
